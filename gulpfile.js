82ae8972b865d07af05c193ec1d6a10b6e825b4e
JungHanYoung <8735132@naver.com>
2020-08-12 13:20:23 +0900

    project init

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
-function defaultTask(cb) {
-    cb()
-}
+const gulp = require('gulp');
+const postcss = require('gulp-postcss')
+const sass = require('gulp-sass');
+const { dest } = require('gulp');
 
-exports.default = defaultTask
+gulp.task("sass", function() {
+    return gulp.src("src/styles.scss")
+    .pipe(postcss([
+        require("tailwindcss"),
+        require("autoprefixer")
+    ]))
+    .pipe(sass({outputStyle: "compact"}))
+    .pipe(gulp.dest("dist"))
+})
+
+gulp.task("sass:watch", function() {
+    return gulp.watch('./src/*.scss', gulp.series(['sass']))
+})
+
